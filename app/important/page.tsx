58c4878563b4a02c66b5efa962db50eed05c64e3
rahul2004a <kumar.rahul.2017354@gmail.com>
2024-01-23 23:39:37 +0530

    project setup and sidebar

---
+++ app/important/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import React from 'react'
+
+const Important = () => {
+    return (
+        <div>important</div>
+    )
+}
+
+export default Important
---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+import Image from "next/image";
+import Tasks from "./components/Tasks/Tasks";
+
+export default function Home() {
+  return <Tasks />;
+}
